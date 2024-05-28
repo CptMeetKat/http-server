a9cb7c4dc688f81ec05406730e8da369ff19ccc3
Reece <reece.cares@gmail.com>
2024-05-28 17:51:38 +1000

    Read HTML file from file system, and send via HTTPRequest

--- src/main/java/MK/HTTPServer/RequestProcessor.java
+++ src/main/java/MK/HTTPServer/RequestProcessor.java
@@ -1,5 +1,7 @@
 package MK.HTTPServer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
@@ -12,6 +14,11 @@ public class RequestProcessor
     {
         //writeOK(client);
         writeHTML(client);
+
+        String currentDirectory = System.getProperty("user.dir");
+        
+        // Print the current directory
+        System.out.println("Current directory: " + currentDirectory);
     }
 
     private static void writeOK(SocketChannel client)
@@ -28,9 +35,30 @@ public class RequestProcessor
     private static void writeHTML(SocketChannel client)
         throws IOException
     {
-        String httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 37\r\n\r\n<html><button>BUTTON1</button></html>";
+        //String httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 37\r\n\r\n<html><button>BUTTON1</button></html>";
+        //byte[] byteArray = httpResponse.getBytes(StandardCharsets.UTF_8);
+        //ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+        //client.write(byteBuffer);
+
+        File htmlFile = new File("./mysample.html"); // Replace "path/to/your/file.html" with the actual path to your HTML file
+        byte[] htmlBytes = new byte[(int) htmlFile.length()];
+        FileInputStream fis = new FileInputStream(htmlFile);
+        fis.read(htmlBytes);
+        fis.close();
+        String body = new String(htmlBytes);
+
+
+
+
+        String httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + body.length() + "\r\n\r\n" + body;
         byte[] byteArray = httpResponse.getBytes(StandardCharsets.UTF_8);
         ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
         client.write(byteBuffer);
+
+
+
+        
+//        ByteBuffer byteBuffer = ByteBuffer.wrap(htmlBytes);
+//        client.write(byteBuffer);
     }
 }
