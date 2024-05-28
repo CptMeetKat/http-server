7a337173e137e3a0a8811c2f7980e90aed6010e5
Reece <reece.cares@gmail.com>
2024-05-28 16:30:52 +1000

    Switched to high test framework version

---
+++ src/test/java/MK/HTTPServer/Basic_Test.java
@@ -0,0 +1,13 @@
+
+package MK.HTTPServer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Basic_Test{
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
+
