31ac44e6d8644d98ba7920e4dc9654008ff0ae05
Reece <reece.cares@gmail.com>
2024-05-29 14:18:39 +1000

    Check if file exists

--- src/main/java/MK/HTTPServer/RequestProcessor.java
+++ src/main/java/MK/HTTPServer/RequestProcessor.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RequestProcessor
 {
@@ -13,7 +16,10 @@ public class RequestProcessor
         throws IOException
     {
         //writeOK(client);
-        writeHTML(client);
+        if( fileExists(request) ) //!directory navigation vuln -> Ennumerate all static files to serve and map
+            writeHTMLFile(client, request);
+        else
+            writeNotFound(client);
 
         String currentDirectory = System.getProperty("user.dir");
         
@@ -29,18 +35,33 @@ public class RequestProcessor
         ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
         client.write(byteBuffer);
     }
-    
 
-
-    private static void writeHTML(SocketChannel client)
+    private static void writeNotFound(SocketChannel client)
         throws IOException
     {
-        //String httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 37\r\n\r\n<html><button>BUTTON1</button></html>";
-        //byte[] byteArray = httpResponse.getBytes(StandardCharsets.UTF_8);
-        //ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
-        //client.write(byteBuffer);
+        String httpResponse = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\n404 Not Found";
+        byte[] byteArray = httpResponse.getBytes(StandardCharsets.UTF_8);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
+        client.write(byteBuffer);
+    }       
 
-        File htmlFile = new File("./mysample.html"); // Replace "path/to/your/file.html" with the actual path to your HTML file
+    private static boolean fileExists(HTTPRequest request)
+    {
+
+        Path filePath = Paths.get(  request.getField("URI")   );
+        System.out.printf("Does Exist: %s\n", filePath);
+        return Files.exists(filePath);
+    }
+
+
+    private static void writeHTMLFile(SocketChannel client, HTTPRequest request)
+        throws IOException
+    {
+        String path = request.getField("URI");
+
+        System.out.printf("GET file: %s\n", path);
+
+        File htmlFile = new File(path); // Replace "path/to/your/file.html" with the actual path to your HTML file
         byte[] htmlBytes = new byte[(int) htmlFile.length()];
         FileInputStream fis = new FileInputStream(htmlFile);
         fis.read(htmlBytes);
@@ -54,11 +75,5 @@ public class RequestProcessor
         byte[] byteArray = httpResponse.getBytes(StandardCharsets.UTF_8);
         ByteBuffer byteBuffer = ByteBuffer.wrap(byteArray);
         client.write(byteBuffer);
-
-
-
-        
-//        ByteBuffer byteBuffer = ByteBuffer.wrap(htmlBytes);
-//        client.write(byteBuffer);
     }
 }
