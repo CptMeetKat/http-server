655871fca0338e558421339d3cadab8abf25ea1f
Reece <reece.cares@gmail.com>
2024-06-07 18:30:23 +1000

    Removed old comments

--- src/main/java/MK/HTTPServer/RequestRouter.java
+++ src/main/java/MK/HTTPServer/RequestRouter.java
@@ -1,8 +1,6 @@
 package MK.HTTPServer;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -45,20 +43,6 @@ public class RequestRouter extends BaseHTTPHandler
 
             System.out.println("Handling dynamic request");
             connection_manager.registerClientSocket("localhost", 8000, new ApplicationServerOperations(context.getHTTPRequest(), context.getSender()));
-            
-
-
-//            String result = DynamicApplication.sendRequest(context.getHTTPRequest());
-//            SocketChannel sc = context.getSender();
-//            try
-//            {
-//                sc.write(  ByteBuffer.wrap(result.getBytes())  );
-//            }
-//            catch(IOException e)
-//            {
-//                System.out.println("ERROR: Could not send the routed response");
-//            }
-            
         }
         else
         {
@@ -70,45 +54,4 @@ public class RequestRouter extends BaseHTTPHandler
         // Print the current directory
         System.out.println("Current directory: " + currentDirectory);
     }
-
-
-
-//    public void processRequest(HTTPHandlerContext context)
-//    {
-//
-//        HTTPRequest request = context.getHTTPRequest();
-//
-//        Path basePath = Paths.get(context.getStaticRoot());
-//        Path userPath = Paths.get("./" + request.getField("URI"));
-//
-//        Path resolvedPath = PathUtils.resolvePath(basePath, userPath);
-//        
-//        Path application1Path = Paths.get(basePath.toString(), "dynamic"); 
-//
-//        //This needs to go.
-//        if(resolvedPath.startsWith(application1Path.toString()))
-//        {
-//            System.out.println("Handling dynamic request");
-//            String result = DynamicApplication.sendRequest(context.getHTTPRequest());
-//            SocketChannel sc = context.getSender();
-//            try
-//            {
-//                sc.write(  ByteBuffer.wrap(result.getBytes())  );
-//            }
-//            catch(IOException e)
-//            {
-//                System.out.println("ERROR: Could not send the routed response");
-//            }
-//            
-//        }
-//        else
-//        {
-//            this.next.processRequest(context);
-//        }
-//
-//        String currentDirectory = System.getProperty("user.dir");
-//        
-//        // Print the current directory
-//        System.out.println("Current directory: " + currentDirectory);
-//    }
 }
