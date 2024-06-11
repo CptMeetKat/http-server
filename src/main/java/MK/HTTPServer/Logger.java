589a1cf13a799684ea7dc2497377ce830c004395
Reece <reece.cares@gmail.com>
2024-06-11 14:48:23 +1000

    Enabled colours for each log level

--- src/main/java/MK/HTTPServer/Logger.java
+++ src/main/java/MK/HTTPServer/Logger.java
@@ -4,13 +4,12 @@ public class Logger
 {
 
     // ANSI escape codes for colors
-    //public static final String RESET = "\033[0m";  // Text Reset
-    //public static final String CYAN = "\033[0;36m";   // CYAN
-    //public static final String BLUE = "\033[0;34m";   // BLUE
-    //public static final String GREEN = "\033[0;32m";  // GREEN
-    //public static final String YELLOW = "\033[0;33m"; // YELLOW
-    //public static final String RED = "\033[0;31m";    // RED
-    //public static final String MAGENTA = "\033[0;35m"; // MAGENTA
+    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String CYAN = "\033[0;36m";   // CYAN
+    public static final String BLUE = "\033[0;34m";   // BLUE
+    public static final String YELLOW = "\033[0;33m"; // YELLOW
+    public static final String RED = "\033[0;31m";    // RED
+    public static final String MAGENTA = "\033[0;35m"; // MAGENTA
 
     public enum PrintLevel
     {
@@ -33,22 +32,22 @@ public class Logger
     {
         switch (level) {
             case TRACE:
-                System.out.println("[TRACE]: " + message);
+                System.out.println(CYAN + "[TRACE]: " + message + RESET);
                 break;
             case DEBUG:
-                System.out.println("[DEBUG]: " + message);
+                System.out.println(BLUE + "[DEBUG]: " + message + RESET);
                 break;
             case INFO:
                 System.out.println("[INFO]: " + message);
                 break;
             case WARNING:
-                System.out.println("[WARNING]: " + message);
+                System.out.println(YELLOW + "[WARNING]: " + message + RESET);
                 break;
             case ERROR:
-                System.err.println("[ERROR]: " + message);  // Using System.err for errors
+                System.err.println(RED + "[ERROR]: " + message + RESET);  // Using System.err for errors
                 break;
             case CRITICAL:
-                System.out.println("[CRITICAL]: " + message);
+                System.out.println(MAGENTA + "[CRITICAL]: " + message + RESET);
                 break;
             default:
                 System.out.println("[UNKNOWN]: " + message);
