721ad79087d16b2b8d0e897ad39189ffd4ddcaf2
Reece <reece.cares@gmail.com>
2024-06-11 15:12:02 +1000

    Added method for setting static logger

--- src/main/java/MK/HTTPServer/Logger.java
+++ src/main/java/MK/HTTPServer/Logger.java
@@ -3,13 +3,6 @@ package MK.HTTPServer;
 public class Logger
 {
     public static Logger logger;
-    public static Logger getLogger()
-    {
-        if(logger == null)
-            return new Logger();
-        return logger;
-    }
-
     private PrintLevel minimum_level = PrintLevel.TRACE;
     // ANSI escape codes for colors
     public static final String RESET = "\033[0m";  // Text Reset
@@ -18,6 +11,21 @@ public class Logger
     public static final String YELLOW = "\033[0;33m"; // YELLOW
     public static final String RED = "\033[0;31m";    // RED
     public static final String MAGENTA = "\033[0;35m"; // MAGENTA
+                                                       
+                                                       
+                                                       
+    public static Logger getLogger()
+    {
+        if(logger == null)
+            return new Logger();
+        return logger;
+    }
+
+    public static void setLogger(PrintLevel level)
+    {
+        logger = new Logger(level);
+    }
+
 
     public enum PrintLevel
     {
