d612d3049a5c6f7fa59e213122bc7ac5fb7ca761
Reece <reece.cares@gmail.com>
2024-06-19 17:34:48 +1000

    Removed deprecated junit tags

--- src/test/java/MK/HTTPServer/AcceptanceTests/Basic_Test.java
+++ src/test/java/MK/HTTPServer/AcceptanceTests/Basic_Test.java
@@ -3,12 +3,9 @@ package MK.HTTPServer.AcceptanceTests;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.internal.runners.JUnit4ClassRunner;
-import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @Category(SlowTests.class)
-@RunWith(JUnit4ClassRunner.class)
 public class Basic_Test{
 
     @Test
