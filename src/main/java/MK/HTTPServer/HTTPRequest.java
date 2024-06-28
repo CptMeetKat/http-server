f44ca1fffa9d3a5e5439e69d9c7da31bdb1b8dfd
Reece <reece.cares@gmail.com>
2024-06-28 17:31:20 +1000

    Improving the parseing of HTTPRequests

--- src/main/java/MK/HTTPServer/HTTPRequest.java
+++ src/main/java/MK/HTTPServer/HTTPRequest.java
@@ -7,15 +7,39 @@ import java.util.HashMap;
 public class HTTPRequest 
 {
     String original_request;
+    String method = "";
+    String uri = "";
+    String version = "";
+    String body = "";
 
     HashMap<String,String> request_map = new HashMap<>();
     public HTTPRequest(String request)
     {
-        setRequest(request);
+        parseRequest(request);
         this.original_request = request;
     }
 
-    private void setRequest(String request)
+    public void setBody(String body)
+    {
+        this.body = body;
+    }
+
+    public void setMethod(String method) //TODO: Only allow the standard methods
+    {
+        this.method = method.toUpperCase();
+    }
+
+    public void setURI(String uri)
+    {
+        this.uri = uri;
+    }
+
+    public void setVersion(String version)
+    {
+        this.version = version;
+    }
+
+    private void parseRequest(String request)
     {
         String[] lines = request.split("\n");
 
@@ -24,7 +48,6 @@ public class HTTPRequest
         request_map.put("URI".toLowerCase(), request_line[1]);
         request_map.put("version".toLowerCase(), request_line[2]);
         
-
         for (int i = 1; i < lines.length; i++)
         {
             String l = lines[i];
@@ -37,20 +60,29 @@ public class HTTPRequest
         }
     }
 
-    public String getField(String fieldname)
+    public String getField(String field)
     {
-        String lower_case_field = fieldname.toLowerCase();
-        return request_map.get(lower_case_field);
+        return request_map.get(field.trim().toLowerCase());
+    }
+
+    public void setField(String field, String value)
+    {
+        request_map.put(field.trim().toLowerCase(),
+                    value.trim());
     }
 
     @Override
     public String toString()
     {
         StringBuilder result = new StringBuilder();
+        result.append(method + " " + uri + " " + version + "\r\n");
+
         for(String field : request_map.keySet())
         {
-            result.append( field + ":" + getField(field) + "\n");
+            result.append( field + ":" + getField(field) + "\r\n");
         }
+        result.append("\r\n");
+        result.append(body);
         return result.toString();
     }
 
