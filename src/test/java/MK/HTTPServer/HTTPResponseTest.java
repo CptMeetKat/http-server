4516df78dc9819cbb0fe5256a926a01c50c73981
Reece <reece.cares@gmail.com>
2024-06-28 16:45:08 +1000

    Added tests for HTTPResponse

---
+++ src/test/java/MK/HTTPServer/HTTPResponseTest.java
@@ -0,0 +1,41 @@
+
+package MK.HTTPServer;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+public class HTTPResponseTest{
+    @Test
+    public void should_parse_HTTP_string_to_object() {
+
+        String input = "HTTP/1.1 200 OK\r\n"
+                    + "Server: SimpleHTTP/0.6 Python/3.10.12\r\n"
+                    + "Content-type: text/html\r\n"
+                    + "Content-Length: 83\r\n"
+                    + "Last-Modified: Wed, 05 Jun 2024 05:29:34 GMT\r\n\r\n"
+                    + "<html><body>This is my file, if u see this - then somthing is working</body></html>";
+
+        HTTPResponse proposed = new HTTPResponse(input);
+
+        HTTPResponse expected = new HTTPResponse();
+        expected.setVersion("1.1");
+        expected.setStatusCode("200");
+        expected.setReasonPhrase("OK");
+        expected.setField("Server", "SimpleHTTP/0.6 Python/3.10.12");
+        expected.setField("Content-type", "text/html");
+        expected.setField("Content-Length", "83");
+        expected.setField("Last-Modified", "Wed, 05 Jun 2024 05:29:34 GMT");
+        expected.setBody("<html><body>This is my file, if u see this - then somthing is working</body></html>");
+
+        assertTrue(proposed.equals(expected)); 
+        //When compared like this the assert does not reveal what field is actually problematic
+    }
+}
+
